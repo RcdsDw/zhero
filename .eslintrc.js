b4bd2b7a9465899a3fea3bbe3a167d3fc1b675c4
Rafael CDS <rafael.candidodasilva.pro@gmail.com>
2024-09-01 17:11:33 +0200

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+// .eslintrc.js
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    semi: ['error', 'always'],
+    'no-console': ['error', { allow: ['error'] }],
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'build/',
+    'dist/',
+    'lib/',
+    'index.ts',
+    'deploy-commands.js',
+  ],
+};
---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Utiliser l'image officielle Node.js comme image de base
+FROM node:18
+
+# Créer un répertoire de travail dans le conteneur
+WORKDIR /usr/src/app
+
+# Copier les fichiers package.json et package-lock.json
+COPY package*.json ./
+
+# Installer les dépendances
+RUN npm install
+
+# Copier le reste des fichiers du projet dans le conteneur
+COPY . .
+
+# Exposer le port sur lequel l'application écoute
+EXPOSE 3333
+
+# Commande à exécuter au démarrage du conteneur
+CMD ["npm", "run", "dev"]
