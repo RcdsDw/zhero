5a57824f93d4e95ae69b4b686e3ea6b0dc994a3b
Alexis Migniau <migniau.alexis@gmail.com>
2024-09-15 13:13:22 +0200

    fix(shop) : Ajout vérif, si celui qui clique c'est bien son shop

--- src/buttons/item/Buy.ts
+++ src/buttons/item/Buy.ts
@@ -3,8 +3,6 @@ import { UserModel } from '../../models/user/user';
 export const id = /ShopBuy/i;
 
 export async function execute(interaction: ButtonInteraction) {
-    const args = interaction.customId.split('-');
-
     const user = await UserModel.findByDiscordUser(interaction.user);
 
     if (!user) {
@@ -14,6 +12,17 @@ export async function execute(interaction: ButtonInteraction) {
         });
         return;
     }
+
+    // On doit verifique que celui qui clique, c'est bien son shop
+    if(interaction.user.id !== interaction.message.mentions.parsedUsers.first()?.id) {
+        interaction.reply({
+            content: "Ce n'est pas votre shop !",
+            ephemeral : true
+        })
+        return;
+    }
+
+    const args = interaction.customId.split('-');
 
     interaction.reply(await user.buyItemFromShop(parseInt(args[1])));
 }
