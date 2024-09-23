3d8d9309aca02a189de4a8f37da4f54b738b3fc9
RcdsDw <rafael.candidodasilva.pro@gmail.com>
2024-09-23 14:52:56 +0200

    fix json

--- src/libs/json/mobs-json.ts
+++ src/libs/json/mobs-json.ts
@@ -1,24 +1,45 @@
 import fs from 'fs';
-import path from 'path'
-import pathNpc from '../../../images/npcs/npc'
+import path from 'path';
 
-fs.readdir(pathNpc, (files: File[]) => {
-    const mobs: any[] = []
+const pathNpc = path.join(__dirname, '../../../images/npcs/npc')
+
+fs.readdir(pathNpc, (err, files) => {
+    if (err) {
+        console.error('Error reading directory:', err);
+        return;
+    }
+
+    const mobs: Object[] = []
     files.forEach(file => {
-        const fileName = path.parse(file).base;
+        const stringifyFile = file.toString()
+        const fileName = path.parse(stringifyFile).base;
         const args = fileName.split('.')
-        const mob: any = {
-            avatar: file,
+        const level = Math.floor(Math.random() * 400)
+        const mob: Object = {
+            id: stringifyFile,
             name: args[0],
-            lvl: Math.floor(Math.random() * 400),
+            lvl: level,
+            skin: path.join(pathNpc, stringifyFile),
+            attributes: {
+                "strength": level + Math.floor(Math.random() * 10),
+                "health": level + Math.floor(Math.random() * 10),
+                "dodge": level + Math.floor(Math.random() * 10),
+                "armor": level + Math.floor(Math.random() * 10),
+                "critical": level + Math.floor(Math.random() * 10),
+                "speed": level + Math.floor(Math.random() * 10)
+            }
         }
 
         mobs.push(mob)
     })
 
-    fs.writeFile('test.json', mobs, (err: Error) => {
+    console.log("Mob 1 : ", mobs[0])
+
+    fs.writeFile('mobs.json', JSON.stringify(mobs, null, 4), (err: NodeJS.ErrnoException | null) => {
         if (err) {
             console.log('Error :', err)
+        } else {
+            console.log('File written successfully');
         }
     })
 })
