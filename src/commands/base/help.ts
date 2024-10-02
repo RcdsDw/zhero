41dc70f2dbb5a6dd5d15cc5f27a60b1205900caf
RcdsDw <rafael.candidodasilva.pro@gmail.com>
2024-10-02 14:52:43 +0200

    fix(style): embed helper

--- src/commands/base/help.ts
+++ src/commands/base/help.ts
@@ -1,23 +1,25 @@
 import { CommandInteraction, SlashCommandBuilder, EmbedBuilder } from 'discord.js';
-import { UserModel } from '../../models/user/user';
 import { Command } from '../../interfaces/command';
 import { getCommands } from '../../libs/discord/getCommands';
 
 export const data = new SlashCommandBuilder().setName('help').setDescription('Résumé des commandes');
 
 export async function execute(interaction: CommandInteraction) {
-
+    const logo: string = "https://img.freepik.com/free-vector/cute-boy-super-hero-flying-cartoon-vector-icon-illustration-people-holiday-icon-concept-isolated_138676-5451.jpg"
+    const thumbnail: string = "https://cdn.dribbble.com/users/154752/screenshots/1244719/book.gif"
     const commands: Command[] = (await getCommands());
-    const embed = new EmbedBuilder();
-    embed.setTitle("📖 Les commandes ! 📖")
-    embed.setDescription("Voici le résumé des commandes actuellement disponibles.")
+    const embed = new EmbedBuilder()
+        .setTitle("Les commandes !")
+        .setDescription("Voici le résumé des commandes actuellement disponibles.\n\n")
+        .setThumbnail(`${thumbnail}`)
     commands.forEach(command => {
         embed.addFields({
-            name: `${command.data.name.slice(0,1).toUpperCase()}${command.data.name.slice(1)}`,
+            name: `${command.data.name.slice(0,1).toUpperCase()}${command.data.name.slice(1)} - /${command.data.name}`,
             value: `${command.data.description}`,
-            inline: true,
         })
     });
+    embed.setTimestamp()
+    embed.setFooter({ text: "L'équipe ZHero", iconURL: `${logo}` });
 
     interaction.reply({ embeds: [embed] })
 }
