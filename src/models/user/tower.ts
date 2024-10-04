de8e9836928032ba49872caa96714efe8f4c6593
RcdsDw <rafael.candidodasilva.pro@gmail.com>
2024-10-04 11:31:40 +0200

    update tower model

--- src/models/user/tower.ts
+++ src/models/user/tower.ts
@@ -1,25 +1,70 @@
 import { HydratedDocument, model, Model, Schema } from 'mongoose';
+import Mob from '../../libs/mobs/Mob';
+import { User } from './user';
 
 // Données du document
 export interface ITower {
-    maxStage: number
-    currentStage: number
+    id: string;
+    maxStage: number;
+    currentStage: number;
+    isCurrentStageBig: boolean;
+    currentMob: Mob;
+    rewardGold: number;
 }
 
 // Méthodes sur l'instance
 interface ITowerMethods {
-    static: () => void;
+    sendRewards(user: User, rewardGold: number, currentStage: number): void;
 }
 
 // Méthodes statiques
 interface ITowerModel extends Model<ITower, object, ITowerMethods> {
-    static: () => void;
+    isBigStage(currentStage: number, isCurrentStageBig: boolean): void;
 }
 
 export type TowerModule = HydratedDocument<ITower, ITowerMethods>;
 
 export const TowerSchema: Schema = new Schema<ITower, object, ITowerMethods>({
+    id: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    maxStage: {
+        type: Number,
+        required: true,
+        default: 100,
+    },
+    currentStage: {
+        type: Number,
+        required: true,
+        default: 1,
+    },
+    isCurrentStageBig: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    currentMob: {
+        type: Mob,
+        required: true,
+        default: () => ({})
+    },
+    rewardGold: {
+        type: Number,
+        required: true,
+        default: 0,
+    }
 });
+
+TowerSchema.static.isBigStage = (currentStage: number, isCurrentStageBig: boolean): void => {
+    isCurrentStageBig = currentStage % 10 === 0;
+}
+
+TowerSchema.methods.sendRewards = (user: User, rewardGold: number, currentStage: number): void => {
+    user.gold += rewardGold;
+    currentStage >= 100 ? null : currentStage++
+};
 
 const TowerModel = model<ITower, ITowerModel>('Tower', TowerSchema);
 
